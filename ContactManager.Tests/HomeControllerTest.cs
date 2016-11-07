305a729c52d3a8d20ab6f2fb9861a12bf60e6f43
Srujan Kurumella <kurumellasrujan@live.com>
2016-11-07 15:16:53 -0700

    Unit Test Project Added

---
+++ ContactManager.Tests/HomeControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContactManager.Controllers;
+using System.Web.Mvc;
+
+namespace ContactManager.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void About_Message_Check()
+        {
+            HomeController controller = new HomeController();
+            var ar = controller.About() as ViewResult;
+            Assert.AreEqual("Your application description page.", ar.ViewData["Message"]);
+        }
+
+        [TestMethod]
+        public void Index_Action_Should_Return_Index_View()
+        {
+            var controller = new HomeController();
+            var indexResult = controller.Index() as ViewResult;
+            Assert.AreEqual(indexResult.ViewName, "");
+        }
+    }
+}
